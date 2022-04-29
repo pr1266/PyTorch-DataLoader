53a731e75e526a325b7a1f80b050a945fe6bc688
pourya <pourya_1999@yahoo.com>
2022-04-29 23:19:02 +0430

    'padd'

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 import pandas as pd
-# from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 class DataLoader:
 
@@ -10,21 +10,42 @@ class DataLoader:
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.n_samples = data.shape[0]
+        temp = (1,)
+        self.sample_shape = np.array(temp + data.shape[1:])
+        # temp = tuple()
+        # temp.append(1)
+        # for i in self.sample_shape:
+        #     temp.append(i)
+        # print(temp)
+        print(self.sample_shape)
         self.prepare()
 
-    def prepare(self):
+    def split_to_batch(self):
         data_array = []        
         for i in range(0, self.n_samples, self.batch_size):            
             step = self.batch_size
             if i + self.batch_size > self.n_samples:
                 step = self.n_samples - i
-            print(step)
             data_array.append(self.data[i:i+step])
-        data_array = np.array(data_array, dtype = object)
-        print(data_array.shape)
+        return data_array
+
+    def padding(self):
+        for i in self.data_array:
+            if i.shape[0] != self.batch_size:
+                print(i.shape)
+                for j in range(i.shape[0], self.batch_size):
+                    padd_data = np.zeros(self.sample_shape)                         
+                    i = np.append(i, padd_data)
+                print(i.shape)
 
 
-x = np.random.rand(100, 32, 32)
+    def prepare(self):
+        
+        self.data_array = self.split_to_batch()
+        self.padded_data = self.padding()
+
+
+x = np.random.rand(100, 32, 32, 3)
 d = DataLoader(x)
 
         
