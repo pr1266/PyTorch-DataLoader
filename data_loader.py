1faf2dc9f5558415fc8d9db5492c1217b8ebde64
pourya <pourya_1999@yahoo.com>
2022-04-29 22:52:32 +0430

    'changes'

--- data_loader.py
+++ data_loader.py
@@ -1,20 +1,26 @@
 import torch
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 
 class DataLoader:
 
-    def __init__(self, data, batch_size, shuffle):
+    def __init__(self, data, batch_size = 2, shuffle = True):
         self.data = data
         self.batch_size = batch_size
         self.shuffle = shuffle
-        self.n_samples = len(data)
+        self.n_samples = data.shape[0]
         self.prepare()
 
     def prepare(self):
-
+        data_array = []
         for i in range(0, self.n_samples, self.batch_size):
-            
+            data_array.append(self.data[i:i+self.batch_size])
+        data_array = np.array(data_array)
+        print(data_array.shape)
+
+
+x = np.random.rand(10).reshape(10, 1)
+d = DataLoader(x)
 
         
