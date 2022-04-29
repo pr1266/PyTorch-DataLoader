49d75321afc1f583b7d20cd059d97d305fb92e69
pourya <pourya_1999@yahoo.com>
2022-04-29 22:47:48 +0430

    'class'

--- data_loader.py
+++ data_loader.py
@@ -5,10 +5,16 @@ from sklearn.model_selection import train_test_split
 
 class DataLoader:
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, data, batch_size, shuffle):
+        self.data = data
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.n_samples = len(data)
+        self.prepare()
 
-    def split(self):
+    def prepare(self):
 
-        x_tr
+        for i in range(0, self.n_samples, self.batch_size):
+            
+
+        
